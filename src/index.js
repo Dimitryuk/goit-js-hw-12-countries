1aa74cd14bbe16916ccc19104cebbf6c9c9d79f9
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-08-18 15:39:34 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import fetchCountries from "./fetchCountries";
 import refs from "./refs";
 import template from './templates/template.hbs'
+import { debounce } from "debounce";
+import {  defaultModules,error} from '@pnotify/core';
+import * as PNotifyDesktop from '@pnotify/desktop/dist/PNotifyDesktop';
 
+import "@pnotify/core/dist/PNotify.css" 
+import "@pnotify/desktop/dist/PNotifyDesktop" ;
+import '@pnotify/core/dist/BrightTheme.css';
+
+ 
+// const myError = error({
+//   text: "I'm an error message."
+// });
+
+refs.input.addEventListener('input', debounce(onInputChange, 500))
+
+const onInputChange = function (event) {
+    event.preventDefault
+    const inputValue = event.target.value
+    console.log(inputValue);
+}
